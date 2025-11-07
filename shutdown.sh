ab0b5179a63a0741d92768c8919b181c17dc1ccc
KernelTuxDistro <kernel.tux-2025@protonmail.com>
2025-11-07 06:38:44 +0000

    Add files via upload

---
+++ shutdown.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+choice=$(printf "Yes\nNo" | rofi -dmenu -i -p "Shutdown Termux X11?")
+
+if [ "$choice" = "Yes" ]; then
+    current_pid=$$
+    pids=$(pgrep -f 'termux.x11' | grep -v $current_pid)
+    if [ -n "$pids" ]; then
+        kill -9 $pids
+    fi
+fi
